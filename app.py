d4919e59cc76f5ffc6b9320ef856b9d1fd6f704f
Sajal <23001001107sajal@dcrustm.org>
2026-02-05 12:54:47 +0530

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'New York, NY',
+        'salary': '$120,000 - $150,000',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'San Francisco, CA',
+        'salary': '$130,000 - $160,000',
+    },
+    {
+        'id': 3,
+        'title': 'Product Manager',
+        'location': 'Austin, TX',
+       
+    },
+    {
+        'id': 4,
+        'title': 'UX Designer',
+        'location': 'Seattle, WA',
+        'salary': '$100,000 - $130,000',
+    },
+
+
+]
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html', jobs=JOBS, company_name='TechCorp')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 
 
